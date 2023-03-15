a1f6bea23b4be7697ab85bebedc16af3619e3f95
TukangM <91467886+TukangM@users.noreply.github.com>
2023-03-15 19:22:49 +0700

    update old cldflr dns to adblock dns

--- config.sh
+++ config.sh
@@ -105,7 +105,7 @@ resolve_conf() {
 	
 	if [ -a /system/etc/resolv.conf ]; then
 		mkdir -p $MODPATH/system/etc/
-		printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" >> $MODPATH/system/etc/resolv.conf
+		printf "nameserver 94.140.14.14\nnameserver 94.140.15.15" >> $MODPATH/system/etc/resolv.conf
 		touch $MODPATH/auto_mount
 	fi
 }
